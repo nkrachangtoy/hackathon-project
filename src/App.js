cecc0f98140364896b0abac7401eff9e0c2e731d
Nusorn <nkrachangtoy@gmail.com>
2020-05-29 11:50:21 -0700

    Add header html and header styling

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./style/main.css";
 
+// COMPONENTS //
+import Header from "./components/Header";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,40 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+
+// ICONS //
+import logo from "../assets/Logo/delta-logo.svg";
+import facebook from "../assets/Icons/facebook-icon.svg";
+import twitter from "../assets/Icons/twitter-icon.svg";
+import instagram from "../assets/Icons/instagram-icon.svg";
+
+function Header() {
+  return (
+    <header className="site-header">
+      <NavLink to="/" className="site-header__navlink">
+        <img src={logo} alt="CHANGE ROOM" />
+      </NavLink>
+      <nav className="nav">
+        <div className="nav__social-media-wrapper">
+          <a href="www.instagram.com">
+            <img src={instagram} className="nav__icons" alt="Instagram" />
+          </a>
+          <a href="www.twitter.com">
+            <img src={twitter} className="nav__icons" alt="Instagram" />
+          </a>
+          <a href="www.facebook.com">
+            <img src={facebook} className="nav__icons" alt="Instagram" />
+          </a>
+        </div>
+        <div className="nav__list-wrapper">
+          <Link className="nav__list">HOME</Link>
+          <Link className="nav__list">VR SHOPPING</Link>
+          <Link className="nav__list">EXPERIENCE</Link>
+          <Link className="nav__list">TESTIMONTIALS</Link>
+          <Link className="nav__list">CONTACT</Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
